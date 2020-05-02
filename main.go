a33a82105515727c3fb34926c0fd941e926ba1bf
Evgen <evgenprogrammer@gmail.com>
2020-05-02 18:26:40 +0300

    add route

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
-import(
+import (
 	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request)  {
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func main()  {
-	
+//Add a showSnippet handler function
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+//Add a createSnippet handler function
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		//Use the http.Error()function to send a 405 status code and "Method not allowed"
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
