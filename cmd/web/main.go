19045e61c015c949518667ba9e285b6050eb5dfb
Evgen <evgenprogrammer@gmail.com>
2020-05-04 20:14:34 +0300

    add flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// flag will be stored in the addr variable at runtime.
+	addr:=flag.String("addr",":4000","HTTP network address")
+	//encountered during parsing the application will be terminated.
+	flag.Parse()
+
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 	// Create a file server which serves files out of the "./ui/static" directory.
-	fileServer := http.FileServer(http.Dir(".ui/static/"))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Println("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
