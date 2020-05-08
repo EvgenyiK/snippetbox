a0ec78bd4a361412774905f66c9fe228d06209d5
Evgen <evgenprogrammer@gmail.com>
2020-05-08 23:08:30 +0300

    app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,13 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct{
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
 
 func main() {
 	// flag will be stored in the addr variable at runtime.
@@ -12,16 +18,31 @@ func main() {
 	//encountered during parsing the application will be terminated.
 	flag.Parse()
 
+	// Create a logger for writing error messages in the same way, but use stderr as
+	infoLog:=log.New(os.Stdout,"INFO\t",log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog:=log.New(os.Stderr,"ERROR\t",log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize a new instance of application containing the dependencies.
+	app:=&application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 	// Create a file server which serves files out of the "./ui/static" directory.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Initialize a new http.Server struct. We set the Addr and Handler
+	srv:=&http.Server{
+		Addr:	*addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
