59039ec65f39d843cb4dfaee5e55add346cad1cc
Evgen <evgenprogrammer@gmail.com>
2020-05-28 17:13:19 +0300

    add users  model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,12 +5,28 @@ import (
 	"time"
 )
 
+//ErrNoRecord error
 var ErrNoRecord = errors.New("models: no matching record found")
+//ErrInvalidCredentials error
+var ErrInvalidCredentials = errors.New("models: invalid credentials")
+//ErrDuplicateEmail error
+var ErrDuplicateEmail = errors.New("models: duplicate email")
 
+//Snippet type
 type Snippet struct {
 	ID      int
 	Title   string
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+// User type with the columns in the database `users` table
+type User struct{
+	ID		int
+	Name	string
+	Email	string
+	HashedPassword	[]byte
+	Created	time.Time
+	Active	bool
 }
