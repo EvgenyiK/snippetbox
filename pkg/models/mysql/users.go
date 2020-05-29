3cf9afb8a0e95f0dc08b915c5f06acbc8e38adbc
Evgen <evgenprogrammer@gmail.com>
2020-05-29 10:57:56 +0300

    add to login in system

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -11,28 +11,28 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type UserModel struct{
+type UserModel struct {
 	DB *sql.DB
 }
 
-func(m *UserModel)Insert(name,email,password string)error{
+func (m *UserModel) Insert(name, email, password string) error {
 	// Create a bcrypt hash of the plain-text password.
-	hashedPassword,err:=bcrypt.GenerateFromPassword([]byte(password),12)
-	if err !=nil {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
 		return err
 	}
 
-	stmt:=`INSERT INTO users(name,email,hashed_password,created)
+	stmt := `INSERT INTO users(name,email,hashed_password,created)
 	VALUES(?, ?, ?, UTC_TIMESTAMP())`
 
 	/*Use the Exec() method to insert the user details and hashed password
-      into the users table.
+	  into the users table.
 	*/
-	_,err=m.DB.Exec(stmt,name,email,string(hashedPassword))
-	if err!=nil {
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
 		var mySQLError *mysql.MySQLError
-		if errors.As(err,&mySQLError) {
-			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message,"users_uc_email"){
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
 				return models.ErrDuplicateEmail
 			}
 		}
@@ -42,10 +42,33 @@ func(m *UserModel)Insert(name,email,password string)error{
 	return nil
 }
 
-func(m *UserModel)Authenticate(email,password string)(int,error){
-	return 0,nil
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ? AND active = TRUE"
+	row := m.DB.QueryRow(stmt, email)
+	err := row.Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	// Check whether the hashed password and plain-text password provided match.
+	// If they don't, we return the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	// Otherwise, the password is correct. Return the user ID.
+	return id, nil
 }
 
-func(m*UserModel)Get(id int)(*models.User,error){
-	return nil,nil
+func (m *UserModel) Get(id int) (*models.User, error) {
+	return nil, nil
 }
