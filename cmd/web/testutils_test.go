8200993174063986de25bc5650e30905932e5d5c
Evgen <evgenprogrammer@gmail.com>
2020-05-30 19:25:08 +0300

    add to testing form

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,63 +1,66 @@
 package main
 
 import (
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
 	"github.com/EvgenyiK/snippetbox/pkg/models/mock"
-	
+
 	"github.com/golangcollege/sessions"
 )
 
 // Create a newTestApplication helper which returns an instance
-func newTestApplication(t *testing.T)*application{
+func newTestApplication(t *testing.T) *application {
 	// Create an instance of the template cache.
-	templateCashe,err:=newTemplateCache("./../../ui/html/")
+	templateCashe, err := newTemplateCache("./../../ui/html/")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Create a session manager instance, with the same settings as production.
-	session:=sessions.New([]byte("3dSm5MnygFHh7XidAtbskXrjbwfoJcbJ"))
+	session := sessions.New([]byte("3dSm5MnygFHh7XidAtbskXrjbwfoJcbJ"))
 	session.Lifetime = 12 * time.Hour
 	session.Secure = true
 
 	// Initialize the dependencies, using the mocks for the loggers and
-    // database models.
+	// database models.
 	return &application{
-		errorLog: log.New(ioutil.Discard,"",0),
-		infoLog: log.New(ioutil.Discard,"",0),
-		session: session,
-		snippets: &mock.SnippetModel{},
+		errorLog:      log.New(ioutil.Discard, "", 0),
+		infoLog:       log.New(ioutil.Discard, "", 0),
+		session:       session,
+		snippets:      &mock.SnippetModel{},
 		templateCache: templateCashe,
-		users:    &mock.UserModel{},
+		users:         &mock.UserModel{},
 	}
 }
 
 // Define a custom testServer type which anonymously embeds a httptest.
-type testServer struct{
+type testServer struct {
 	*httptest.Server
 }
 
 // Create a newTestServer helper which initalizes and returns a new instance
-func newTestServer(t*testing.T,h http.Handler)*testServer{
-	ts:=httptest.NewTLSServer(h)
-	
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+
 	// Initialize a new cookie jar.
-	jar,err:=cookiejar.New(nil)
-	if err != nil{
+	jar, err := cookiejar.New(nil)
+	if err != nil {
 		t.Fatal(err)
 	}
 	// Add the cookie jar to the client, so that response cookies are stored
 	ts.Client().Jar = jar
 
 	// Disable redirect-following for the client.
-	ts.Client().CheckRedirect = func (req *http.Request,via []*http.Request) error {
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
 		return http.ErrUseLastResponse
 	}
 
@@ -65,17 +68,49 @@ func newTestServer(t*testing.T,h http.Handler)*testServer{
 }
 
 // Implement a get method on our custom testServer type.
-func(ts *testServer)get(t *testing.T, urlPath string)(int,http.Header, []byte){
-	rs,err:=ts.Client().Get(ts.URL + urlPath)
-	if err != nil{
-		t.Fatal(err)
-	}
-
-	defer rs.Body.Close()
-	body,err:=ioutil.ReadAll(rs.Body)
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return rs.StatusCode, rs.Header, body
+}
+
+// Define a regular expression which captures the CSRF token value from the
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCRFToken(t *testing.T, body []byte) string {
+	// Use the FindSubmatch method to extract the token from the HTML body.
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
+
+
+// Create a postForm method for sending POST requests to the test server.
+// The final parameter to this method is a url.Values object which can contain
+// any data that you want to send in the request body.
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Read the response body.
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Return the response status, headers and body.
 	return rs.StatusCode, rs.Header, body
 }
