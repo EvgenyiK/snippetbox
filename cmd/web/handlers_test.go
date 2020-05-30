c12c27958f0a08eeb32df6332316c1b06e5a8b0e
Evgen <evgenprogrammer@gmail.com>
2020-05-30 17:34:15 +0300

    add sub-tests

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -20,5 +21,42 @@ func TestPing(t *testing.T){
 
 	if string(body) != "OK"{
 		t.Errorf("want body to equal %q", "OK")
+	}
+}
+
+func TestShowSnippet(t *testing.T){
+	// Create a new instance of our application struct which uses the mocked
+	app:=newTestApplication(t)
+	ts:=newTestServer(t,app.routes())
+	defer ts.Close()
+
+	// Set up some table-driven tests to check the responses sent
+	tests:=[]struct{
+		name string
+		urlPath string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID","/snippet/1",http.StatusOK,[]byte("An old silent pond...")},
+		{"Non-existent ID","/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID","/snippet/-1",http.StatusNotFound,nil},
+		{"Negative ID","/snippet/1.23",http.StatusNotFound,nil},
+		{"Negative ID","/snippet/foo",http.StatusNotFound,nil},
+		{"Negative ID","/snippet/",http.StatusNotFound,nil},
+		{"Negative ID","/snippet/1/",http.StatusNotFound,nil},
+	}
+
+	for _,tt:=range tests{
+		t.Run(tt.name, func(t *testing.T){
+			code, _,body:=ts.get(t,tt.urlPath)
+
+			if code !=tt.wantCode{
+				t.Errorf("want %d;got %d", tt.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tt.wantBody){
+				t.Errorf("want body to contain %d", tt.wantBody)
+			}
+		})
 	}
 }
